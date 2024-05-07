6ce99fc7f91908a8836beba87e78c7fdb2d959d6
Farid Sakhizad <fsakhyzadcf@gmail.com>
2024-05-07 12:35:53 -0600

    test restriction commit 2

--- src/App.js
+++ src/App.js
@@ -18,8 +18,6 @@ function App() {
     setTheme(theme === 'light' ? 'dark' : 'light');
   }
 
-  console.log('ASDF');
-
   return (
     <Provider store={store}>
       <ThemeContext.Provider value={theme}>
